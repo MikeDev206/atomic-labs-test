4e8273f51c1f4ca798fe4e4106a72808f057d138
Miguel Angel Aceves <acevesm77@gmail.com>
2022-03-18 11:03:37 -0600

    modified temporarily

--- src/Components/Hooks/useForm.js
+++ src/Components/Hooks/useForm.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const useForm = (initialForm, validateForm) => {
-	const [form, setForm] = useState(initialForm);
-	const [errors, setErrors] = useState({});
-	const [loading, setLoading] = useState(false);
-	const [response, setResponse] = useState(null);
+	const [form] = useState(initialForm);
+	const [errors] = useState({});
+	const [loading] = useState(false);
+	const [response] = useState(null);
 
 	const handleChange = (e) => {};
 
